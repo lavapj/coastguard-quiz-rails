23f7290982b6cdbbdbd42d27025a5dcb3e80f60e
Patti <pj.97211@gmail.com>
2013-03-31 13:55:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoastguardQuizRails::Application.config.secret_token = '6c52e2a738e215135a08d6acba38d26d9c76775b9ea5d49299527cecb980bb1e55bd7375dfa84c9fd8231a23f0978015ee3d847cc13af9e7d8e2318525b6733e'
